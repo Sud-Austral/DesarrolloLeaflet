39a790844410a6cedeb27e843c2a92775a5d8a8b
Edwin Pineda <61611378+edwinpineda96@users.noreply.github.com>
2021-06-21 16:38:01 -0600

    adelantos

---
+++ AplicacionLogin/AplicacionLogin/Controllers/estructuraController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacionLogin.Controllers
+{
+    public class estructuraController : Controller
+    {
+        // GET: estructura
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult mapa1(int id=10)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+
+
+        public ActionResult mapa2()
+        {
+            return View();
+        }
+
+    }
+}
