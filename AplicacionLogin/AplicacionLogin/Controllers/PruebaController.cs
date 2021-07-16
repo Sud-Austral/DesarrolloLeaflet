9f42e47299f953ed507bc60997d0e78275a81bef
Vivian Daniela Romero <61244319+Viviandrg7@users.noreply.github.com>
2021-07-16 16:37:05 -0600

    menu

---
+++ AplicacionLogin/AplicacionLogin/Controllers/PruebaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacionLogin.Controllers
+{
+    public class PruebaController : Controller
+    {
+        // GET: Prueba
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MenuLateral()
+        {
+            return View();
+        }
+    }
+}
