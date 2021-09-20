4ddcd0082bf8089402671b63ce27cd78594dc841
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-09-20 00:35:47 -0600

    Commit
    Avances de la caracterización biofísica.

---
+++ AplicacionLogin/AplicacionLogin/Controllers/BiofisicaController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacionLogin.Controllers
+{
+    public class BiofisicaController : Controller
+    {
+        // GET: Biofisica
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Biofisica(int comuna = 0, string var = "")
+        {
+            ViewBag.codCom = comuna;
+            ViewBag.variable = var;
+            return View();
+        }
+    }
+}
