c85e366fa58bf5ad90e501da7db9141f3966c086
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-09-28 17:31:46 -0600

    Commit, agregación de las variables climáticas.

---
+++ AplicacionLogin/AplicacionLogin/Controllers/CuidadesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacionLogin.Controllers
+{
+    public class CuidadesController : Controller
+    {
+        // GET: Cuidades
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cuidades(int id = 0)
+        {
+            ViewBag.cuidad = id;
+            return View();
+        }
+    }
+}
